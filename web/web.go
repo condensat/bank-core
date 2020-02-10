2ce3dd3441e95f49a737d1724c946333ff9173b0
Michel Foucault <michel@condensat.tech>
2020-02-10 19:00:00 +0000

    Add BankWebApp
    - serve static webapp files

--- web/web.go
+++ web/web.go
@@ -2,40 +2,34 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
-package api
+package web
 
 import (
 	"context"
 	"fmt"
 	"net"
 	"net/http"
-	"os"
 	"time"
 
-	"github.com/condensat/bank-core/api/services"
-	"github.com/condensat/bank-core/api/sessions"
+	"github.com/condensat/bank-core/api"
 	"github.com/condensat/bank-core/logger"
 
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/negroni"
 )
 
-type Api int
+// Version
+const Version string = "0.1"
 
-func (p *Api) Run(ctx context.Context, port int) {
-	log := logger.Logger(ctx).WithField("Method", "api.Api.Run")
+type Web int
+
+func (p *Web) Run(ctx context.Context, port int, webDirectory string) {
+	log := logger.Logger(ctx).WithField("Method", "web.Web.Run")
 
 	muxer := http.NewServeMux()
 
-	// create session and and to context
-	session := sessions.NewSession(ctx)
-	ctx = context.WithValue(ctx, sessions.KeySessions, session)
-
-	services.RegisterServices(ctx, muxer)
-
-	handler := negroni.New(&negroni.Recovery{})
-	handler.Use(services.StatsMiddleware)
-	handler.UseFunc(MiddlewarePeerRateLimiter)
+	handler := negroni.New(negroni.NewRecovery(), negroni.NewStatic(http.Dir(webDirectory)))
+	handler.UseFunc(api.MiddlewarePeerRateLimiter)
 	handler.UseFunc(AddWorkerHeader)
 	handler.UseFunc(AddWorkerVersion)
 	handler.UseHandler(muxer)
@@ -44,7 +38,7 @@ func (p *Api) Run(ctx context.Context, port int) {
 		Addr:           fmt.Sprintf(":%d", port),
 		Handler:        handler,
 		ReadTimeout:    3 * time.Second,
-		WriteTimeout:   3 * time.Second,
+		WriteTimeout:   30 * time.Second,
 		MaxHeaderBytes: 1 << 16, // 16 KiB
 		ConnContext:    func(conCtx context.Context, c net.Conn) context.Context { return ctx },
 	}
@@ -58,30 +52,22 @@ func (p *Api) Run(ctx context.Context, port int) {
 	}()
 
 	log.WithFields(logrus.Fields{
-		"Hostname": GetHost(),
-		"Port":     port,
-	}).Info("Api Service started")
+		"Hostname":     api.GetHost(),
+		"Port":         port,
+		"WebDirectory": webDirectory,
+	}).Info("WebApp started")
 
 	<-ctx.Done()
 }
 
 // AddWorkerHeader - adds header of which node actually processed request
 func AddWorkerHeader(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	rw.Header().Add("X-Worker", GetHost())
+	rw.Header().Add("X-Worker", api.GetHost())
 	next(rw, r)
 }
 
 // AddWorkerVersion - adds header of which version is installed
 func AddWorkerVersion(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	rw.Header().Add("X-Worker-Version", services.Version)
+	rw.Header().Add("X-Worker-Version", Version)
 	next(rw, r)
-}
-
-func GetHost() string {
-	var err error
-	host, err := os.Hostname()
-	if err != nil {
-		host = "Unknown"
-	}
-	return host
 }
