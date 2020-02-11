ce20891d3f893c41327448832bf5ec0e42acf5a6
Michel Foucault <michel@condensat.tech>
2020-02-11 15:00:00 +0000

    Consider CF-Connecting-IP in RequesterIP

--- api/services/utils.go
+++ api/services/utils.go
@@ -24,11 +24,14 @@ func fromTimestampMillis(timestamp int64) time.Time {
 
 func RequesterIP(r *http.Request) string {
 	// Header added by reverse proxy
-	const xRealIP = "X-Real-Ip"
-	const xForwardedFor = "X-Forwarded-For"
+	const cfConnectingIP = "CF-Connecting-IP" // CloudFlare
+	const xRealIP = "X-Real-Ip"               // traefik
+	const xForwardedFor = "X-Forwarded-For"   // Generic proxy
 
 	// Priority order
-	if ips, ok := r.Header[xRealIP]; ok && len(ips) > 0 {
+	if ips, ok := r.Header[cfConnectingIP]; ok && len(ips) > 0 {
+		return ips[0]
+	} else if ips, ok := r.Header[xRealIP]; ok && len(ips) > 0 {
 		return ips[0]
 	} else if ips, ok := r.Header[xForwardedFor]; ok && len(ips) > 0 {
 		return ips[0]
