a09d510133b3140ff86371491f8a0858642bc4d9
Michel Foucault <michel@condensat.tech>
2020-02-11 11:00:00 +0000

    Server hash login and password

--- database/credential.go
+++ database/credential.go
@@ -6,6 +6,7 @@ package database
 
 import (
 	"context"
+	"encoding/hex"
 	"errors"
 
 	"github.com/condensat/bank-core"
@@ -31,10 +32,18 @@ func saltCredentials(ctx context.Context, login, password string) ([]byte, []byt
 	return loginHash, passwordHash
 }
 
+func HashEntry(entry string) string {
+	hash := utils.HashBytes([]byte(entry))
+	return hex.EncodeToString(hash[:])
+}
+
 func CreateOrUpdatedCredential(ctx context.Context, database bank.Database, userID uint64, login, password, otpSecret string) (*model.Credential, error) {
 	switch db := database.DB().(type) {
 	case *gorm.DB:
 
+		// perform a sha512 hex digest of login and password
+		login = HashEntry(login)
+		password = HashEntry(password)
 		password = login + password // password prefixed with login for uniqueness
 		loginHash := security.SaltedHash(ctx, []byte(login))
 		passwordHash := security.SaltedHash(ctx, []byte(password))
@@ -61,6 +70,10 @@ func CreateOrUpdatedCredential(ctx context.Context, database bank.Database, user
 func CheckCredential(ctx context.Context, database bank.Database, login, password string) (uint64, bool, error) {
 	switch db := database.DB().(type) {
 	case *gorm.DB:
+
+		// client should send a sha512 hex digest of the password
+		// login = hashEntry(login)
+		// password = hashEntry(password)
 
 		password = login + password // password prefixed with login for uniqueness
 		loginHash := security.SaltedHash(ctx, []byte(login))
