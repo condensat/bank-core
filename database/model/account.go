ff30fcc82d85b600f756db00bdfaa9cb85aca21f
Michel Foucault <michel@condensat.tech>
2020-03-20 12:00:00 +0000

    Add Account model

---
+++ database/model/account.go
@@ -0,0 +1,11 @@
+// Copyright 2020 Condensat Tech. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package model
+
+type Account struct {
+	ID           uint64 `gorm:"primary_key"`                     // [PK] Account
+	UserID       uint64 `gorm:"index;not null"`                  // [FK] Reference to User table
+	CurrencyName string `gorm:"index;not null;type:varchar(16)"` // [FK] Reference to Currency table
+}
--- database/model/currency.go
+++ database/model/currency.go
@@ -6,7 +6,7 @@ package model
 
 type Currency struct {
 	Name      string `gorm:"primary_key;type:varchar(16)"` // [PK] Currency
-	Available *int   `gorm:"default:0"`
+	Available *int   `gorm:"default:0;not null"`
 }
 
 func NewCurrency(name string, available int) Currency {
