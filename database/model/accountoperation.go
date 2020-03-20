693ece298288121093b818efff7e771dd54cec7f
Michel Foucault <michel@condensat.tech>
2020-03-20 12:00:00 +0000

    Add AccountOperation model

---
+++ database/model/accountoperation.go
@@ -0,0 +1,80 @@
+// Copyright 2020 Condensat Tech. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package model
+
+import (
+	"math"
+	"time"
+)
+
+type AccountOperationID ID
+
+type SynchroneousType String
+type OperationType String
+
+// AccountOperation model
+type AccountOperation struct {
+	ID        AccountOperationID `gorm:"primary_key"`    // [PK] AccountOperation
+	PrevID    AccountOperationID `gorm:"index;not null"` // [FK] Reference to previous AccountOperation (0 mean first operation)
+	AccountID AccountID          `gorm:"index;not null"` // [FK] Reference to Account table
+
+	SynchroneousType SynchroneousType `gorm:"index;not null;type:varchar(16)"` // [enum] Operation synchroneous type (sync, async_start, async_end)
+	OperationType    OperationType    `gorm:"index;not null;type:varchar(16)"` // [enum] Determine table for ReferenceID (none, other)
+	ReferenceID      RefID            `gorm:"index;not null"`                  // [optional - FK] Reference to related table with OperationType
+
+	Timestamp time.Time `gorm:"index;not null;type:timestamp"` // Operation timestamp
+	Amount    ZeroFloat `gorm:"default:0;not null"`            // Operation amount (can be negative)
+	Balance   ZeroFloat `gorm:"default:0;not null"`            // Account balance (strictly positive or zero)
+
+	LockAmount  ZeroFloat `gorm:"default:0;not null"` // Operation amount (can be negative)
+	TotalLocked ZeroFloat `gorm:"default:0;not null"` // Total locked (strictly positive or zero and less or equal than Balance)
+}
+
+func NewAccountOperation(ID, prevID AccountOperationID, accountID AccountID, synchroneousType SynchroneousType, operationType OperationType, referenceID RefID, timestamp time.Time, amount, balance, lockAmount, totalLocked Float) AccountOperation {
+	return AccountOperation{
+		ID:        ID,
+		PrevID:    prevID,
+		AccountID: accountID,
+
+		SynchroneousType: synchroneousType,
+		OperationType:    operationType,
+		ReferenceID:      referenceID,
+
+		Timestamp: timestamp.UTC(),
+		Amount:    &amount,
+		Balance:   &balance,
+
+		LockAmount:  &lockAmount,
+		TotalLocked: &totalLocked,
+	}
+}
+
+func (p *AccountOperation) IsValid() bool {
+	return p.ID > 0 &&
+		p.ID > p.PrevID &&
+		p.AccountID > 0 &&
+
+		// check enums
+		len(p.OperationType) > 0 &&
+		len(p.SynchroneousType) > 0 &&
+
+		// check pointers
+		p.Amount != nil && p.Balance != nil &&
+		p.LockAmount != nil && p.TotalLocked != nil &&
+
+		// check positive balance and TotalLocked
+		*p.Balance >= 0 &&
+		*p.TotalLocked >= 0 &&
+		// check for max total locked
+		*p.TotalLocked <= *p.Balance &&
+
+		// check amount less or equals than balance
+		*p.Amount <= *p.Balance &&
+		// check lockAmount less or equals than totalLocked
+		*p.LockAmount <= *p.TotalLocked &&
+
+		// Check for void operation
+		(math.Abs(float64(*p.Amount)) > 0.0 || math.Abs(float64(*p.LockAmount)) > 0.0)
+}
---
+++ database/model/accountoperation_test.go
@@ -0,0 +1,124 @@
+// Copyright 2020 Condensat Tech. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package model
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func newFloat(value Float) ZeroFloat {
+	return &value
+}
+
+func refAccountOperation() AccountOperation {
+	return AccountOperation{
+		ID:               1,
+		PrevID:           0,
+		AccountID:        3,
+		SynchroneousType: "SynchroneousType",
+		OperationType:    "OperationType",
+		ReferenceID:      4,
+		Timestamp:        time.Now().UTC().Truncate(time.Second),
+		Amount:           newFloat(1.0),
+		Balance:          newFloat(10.0),
+		LockAmount:       newFloat(2.0),
+		TotalLocked:      newFloat(8.0),
+	}
+}
+
+func amountAccountOperation(amount, balance, lockAmount, totalLocked Float) AccountOperation {
+	result := refAccountOperation()
+	*result.Amount = amount
+	*result.Balance = balance
+	*result.LockAmount = lockAmount
+	*result.TotalLocked = totalLocked
+	return result
+}
+
+func TestNewAccountOperation(t *testing.T) {
+	t.Parallel()
+
+	ref := refAccountOperation()
+
+	type args struct {
+		ID               AccountOperationID
+		prevID           AccountOperationID
+		accountID        AccountID
+		synchroneousType SynchroneousType
+		operationType    OperationType
+		referenceID      RefID
+		timestamp        time.Time
+		amount           Float
+		balance          Float
+		lockAmount       Float
+		totalLocked      Float
+	}
+	tests := []struct {
+		name string
+		args args
+		want AccountOperation
+	}{
+		{"CheckArgsOrder", args{ref.ID, ref.PrevID, ref.AccountID, ref.SynchroneousType, ref.OperationType, ref.ReferenceID, ref.Timestamp, *ref.Amount, *ref.Balance, *ref.LockAmount, *ref.TotalLocked}, ref},
+	}
+	for _, tt := range tests {
+		tt := tt // capture range variable
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewAccountOperation(tt.args.ID, tt.args.prevID, tt.args.accountID, tt.args.synchroneousType, tt.args.operationType, tt.args.referenceID, tt.args.timestamp, tt.args.amount, tt.args.balance, tt.args.lockAmount, tt.args.totalLocked)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewAccountOperation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAccountOperation_IsValid(t *testing.T) {
+	t.Parallel()
+
+	type fields struct {
+		p AccountOperation
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{"Default", fields{AccountOperation{}}, false},
+		{"Valid", fields{NewAccountOperation(1, 0, 42, "none", "sync", 0, time.Now(), 1.0, 1.0, 1.0, 1.0)}, true},
+		{"ValidUTC", fields{NewAccountOperation(1, 0, 42, "none", "sync", 0, time.Now().UTC(), 1.0, 1.0, 1.0, 1.0)}, true},
+
+		{"InvalidID", fields{NewAccountOperation(0, 0, 42, "none", "sync", 0, time.Now(), 1.0, 1.0, 0.0, 0.0)}, false},
+		{"InvalidPrevID", fields{NewAccountOperation(1, 1, 42, "none", "sync", 0, time.Now(), 1.0, 1.0, 0.0, 0.0)}, false},
+		{"InvalidAccountID", fields{NewAccountOperation(1, 0, 0, "none", "sync", 0, time.Now(), 1.0, 1.0, 0.0, 0.0)}, false},
+		{"InvalidSynchroneousType", fields{NewAccountOperation(1, 0, 42, "none", "", 0, time.Now(), 1.0, 1.0, 0.0, 0.0)}, false},
+		{"InvalidOperationType", fields{NewAccountOperation(1, 0, 42, "", "sync", 0, time.Now(), 1.0, 1.0, 0.0, 0.0)}, false},
+
+		// test with amount and lock
+		{"ValidNegativeAmount", fields{amountAccountOperation(-1.0, 1.0, 0.0, 1.0)}, true},
+		{"ValidNegativeTotalLocked", fields{amountAccountOperation(0.0, 1.0, -1.0, 1.0)}, true},
+
+		{"InvalidTotalLockedAndBalance", fields{amountAccountOperation(0.0, 1.0, 0.0, 2.0)}, false},
+		{"InvalidBalance", fields{amountAccountOperation(0.0, -1.0, 0.0, 0.0)}, false},
+		{"InvalidLockTotalLocked", fields{amountAccountOperation(0.0, 1.0, 0.0, -1.0)}, false},
+		{"InvalidTooManyLocked", fields{amountAccountOperation(0.0, 1.0, 0.0, 2.0)}, false},
+
+		{"InvalidBalanceToLow", fields{amountAccountOperation(2.0, 1.0, 0.0, 0.0)}, false},
+		{"InvalidTotalLockedToLow", fields{amountAccountOperation(0.0, 0.0, 2.0, 1.0)}, false},
+
+		// test void operation
+		{"InvalidZero", fields{amountAccountOperation(0.0, 0.0, 0.0, 0.0)}, false},
+		{"InvalidVoidOperation", fields{amountAccountOperation(0.0, 1.0, 0.0, 1.0)}, false},
+	}
+	for _, tt := range tests {
+		tt := tt // capture range variable
+		t.Run(tt.name, func(t *testing.T) {
+			p := tt.fields.p
+			if got := p.IsValid(); got != tt.want {
+				t.Errorf("AccountOperation.IsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
