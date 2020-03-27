17c3d93caeff7fbe3f5618314e7c3c76cae95552
Michel Foucault <michel@condensat.tech>
2020-03-27 09:00:00 +0000

    Implement ListUserAccounts

--- accounting/accountinfo.go
+++ accounting/accountinfo.go
@@ -7,11 +7,41 @@ package accounting
 import (
 	"context"
 	"time"
+
+	"github.com/condensat/bank-core"
+	"github.com/condensat/bank-core/appcontext"
+	"github.com/condensat/bank-core/database"
+	"github.com/condensat/bank-core/database/model"
 )
 
 func ListUserAccounts(ctx context.Context, userID uint64) ([]AccountInfo, error) {
 	var result []AccountInfo
-	return result, nil
+
+	db := appcontext.Database(ctx)
+	err := db.Transaction(func(db bank.Database) error {
+		accounts, err := database.GetAccountsByUserAndCurrencyAndName(db, model.UserID(userID), "", "*")
+		if err != nil {
+			return err
+		}
+
+		for _, account := range accounts {
+			accountState, err := database.GetAccountStatusByAccountID(db, account.ID)
+			if err != nil {
+				return err
+			}
+
+			result = append(result, AccountInfo{
+				AccountID: uint64(account.ID),
+				Currency:  string(account.CurrencyName),
+				Name:      string(account.Name),
+				Status:    string(accountState.State),
+			})
+		}
+
+		return nil
+	})
+
+	return result, err
 }
 
 func GetAccountHistory(ctx context.Context, accountID uint64, from, to time.Time) ([]AccountEntry, error) {
