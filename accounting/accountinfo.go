d016e6ed6e5741750b01ae5a584e93401873d631
Michel Foucault <michel@condensat.tech>
2020-03-27 09:00:00 +0000

    Implement GetAccountHistory

--- accounting/accountinfo.go
+++ accounting/accountinfo.go
@@ -46,5 +46,43 @@ func ListUserAccounts(ctx context.Context, userID uint64) ([]AccountInfo, error)
 
 func GetAccountHistory(ctx context.Context, accountID uint64, from, to time.Time) ([]AccountEntry, error) {
 	var result []AccountEntry
-	return result, nil
+
+	db := appcontext.Database(ctx)
+	err := db.Transaction(func(db bank.Database) error {
+		account, err := database.GetAccountByID(db, model.AccountID(accountID))
+		if err != nil {
+			return err
+		}
+
+		operations, err := database.GeAccountHistoryRange(db, account.ID, from, to)
+		if err != nil {
+			return err
+		}
+
+		for _, op := range operations {
+			if !op.IsValid() {
+				return database.ErrInvalidAccountOperation
+			}
+
+			result = append(result, AccountEntry{
+				AccountID: uint64(op.AccountID),
+				Currency:  string(account.CurrencyName),
+
+				OperationType:    string(op.OperationType),
+				SynchroneousType: string(op.SynchroneousType),
+
+				Timestamp: op.Timestamp,
+				Label:     "N/A",
+				Amount:    float64(*op.Amount),
+				Balance:   float64(*op.Balance),
+
+				LockAmount:  float64(*op.LockAmount),
+				TotalLocked: float64(*op.TotalLocked),
+			})
+		}
+
+		return nil
+	})
+
+	return result, err
 }
