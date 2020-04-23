413e7566373a26ba72b9b1e37f166c34bd49f644
Michel Foucault <michel@condensat.tech>
2020-04-23 16:00:00 +0000

    Add chain params to BitcoinClient
    - port must be 18332 for testnet

--- wallet/bitcoin/client.go
+++ wallet/bitcoin/client.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 
 	"github.com/condensat/bank-core/logger"
+	"github.com/btcsuite/btcd/chaincfg"
 	rpc "github.com/btcsuite/btcd/rpcclient"
 )
 
@@ -21,6 +22,15 @@ var (
 type BitcoinClient struct {
 	conn   *rpc.ConnConfig
 	client *rpc.Client
+	params *chaincfg.Params
+}
+
+func paramsFromRPCPort(port int) *chaincfg.Params {
+	params := &chaincfg.MainNetParams
+	if port == 18332 {
+		params = &chaincfg.TestNet3Params
+	}
+	return params
 }
 
 func New(ctx context.Context, options BitcoinOptions) *BitcoinClient {
@@ -41,6 +51,7 @@ func New(ctx context.Context, options BitcoinOptions) *BitcoinClient {
 	return &BitcoinClient{
 		conn:   connCfg,
 		client: client,
+		params: paramsFromRPCPort(options.Port),
 	}
 }
 
