f292ae2566dbaab21e28522df9299f3a285a4eb6
Michel Foucault <michel@condensat.tech>
2020-04-23 06:00:00 +0000

    Update Chain state periodically
    - every 30s
    - fetch from daemon
    - store into cache

---
+++ wallet/chain.go
@@ -0,0 +1,65 @@
+// Copyright 2020 Condensat Tech. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package wallet
+
+import (
+	"context"
+
+	"github.com/condensat/bank-core/cache"
+	"github.com/condensat/bank-core/logger"
+)
+
+type ChainState struct {
+	Chain  string
+	Height uint64
+}
+
+type AddressInfo struct {
+	Chain         string
+	PublicAddress string
+	Mined         uint64 // 0 unknown, 1 mempool, BlockHeight
+}
+
+func FetchChainsState(ctx context.Context, chains ...string) ([]ChainState, error) {
+	log := logger.Logger(ctx).WithField("Method", "wallet.FetchChainsState")
+
+	var result []ChainState
+	for _, chain := range chains {
+		state, err := fetchChainState(ctx, chain)
+		if err != nil {
+			continue
+		}
+
+		result = append(result, state)
+	}
+
+	log.
+		WithField("Count", len(result)).
+		Debug("Chains State Fetched")
+
+	return result, nil
+}
+
+func fetchChainState(ctx context.Context, chain string) (ChainState, error) {
+	log := logger.Logger(ctx).WithField("Method", "wallet.fetchChainState")
+
+	log = log.WithField("Chain", chain)
+
+	// Acquire Lock
+	lock, err := cache.LockChain(ctx, chain)
+	if err != nil {
+		log.WithError(err).
+			Error("Failed to lock chain")
+		return ChainState{}, cache.ErrLockError
+	}
+	defer lock.Unlock()
+
+	// Todo: RPC call to chain daemon
+
+	return ChainState{
+		Chain:  chain,
+		Height: 42,
+	}, nil
+}
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -6,6 +6,7 @@ package wallet
 
 import (
 	"context"
+	"time"
 
 	"github.com/condensat/bank-core/appcontext"
 	"github.com/condensat/bank-core/logger"
@@ -19,16 +20,25 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+const (
+	DefaultInterval time.Duration = 30 * time.Second
+)
+
 type Wallet int
 
 func (p *Wallet) Run(ctx context.Context, options WalletOptions) {
 	log := logger.Logger(ctx).WithField("Method", "Wallet.Run")
 
-	p.registerHandlers(cache.RedisMutexContext(ctx))
+	// add RedisMutext to context
+	ctx = cache.RedisMutexContext(ctx)
+
+	p.registerHandlers(ctx)
 
 	log.WithFields(logrus.Fields{
 		"Hostname": utils.Hostname(),
 	}).Info("Wallet Service started")
+
+	go p.scheduledUpdate(ctx, options.Chains(), DefaultInterval)
 
 	<-ctx.Done()
 }
@@ -43,4 +53,44 @@ func (p *Wallet) registerHandlers(ctx context.Context) {
 	nats.SubscribeWorkers(ctx, common.CryptoAddressNextDepositSubject, concurencyLevel, handlers.OnCryptoAddressNextDeposit)
 
 	log.Debug("Bank Wallet registered")
+}
+
+func checkParams(interval time.Duration) time.Duration {
+	if interval < time.Second {
+		interval = DefaultInterval
+	}
+	return interval
+}
+
+func (p *Wallet) scheduledUpdate(ctx context.Context, chains []string, interval time.Duration) {
+	log := logger.Logger(ctx).WithField("Method", "Wallet.scheduledUpdate")
+
+	interval = checkParams(interval)
+
+	log = log.WithFields(logrus.Fields{
+		"Interval": interval.String(),
+	})
+
+	log.Info("Start wallet Scheduler")
+
+	for epoch := range utils.Scheduler(ctx, interval, 0) {
+		chainsStates, err := FetchChainsState(ctx, chains...)
+		if err != nil {
+			log.WithError(err).
+				Error("Failed to FetchChainsState")
+			continue
+		}
+
+		log.WithFields(logrus.Fields{
+			"Epoch": epoch.Truncate(time.Millisecond),
+			"Count": len(chainsStates),
+		}).Info("Chain state fetched")
+
+		err = UpdateRedisChain(ctx, chainsStates)
+		if err != nil {
+			log.WithError(err).
+				Error("Failed to UpdateRedisChain")
+			continue
+		}
+	}
 }
