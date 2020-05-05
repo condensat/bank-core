9483caa802563ef7fc851874be7acd193d105b39
Michel Foucault <michel@condensat.tech>
2020-05-05 16:00:00 +0000

    Deposit to user asset account when needed
    - Create user Account for asset if needed
    - error if account checks fails

--- wallet/tasks/updateoperations.go
+++ wallet/tasks/updateoperations.go
@@ -22,7 +22,7 @@ import (
 
 // UpdateOperations
 func UpdateOperations(ctx context.Context, epoch time.Time, chains []string) {
-	log := logger.Logger(ctx).WithField("Method", "task.ChainUpdate")
+	log := logger.Logger(ctx).WithField("Method", "tasks.UpdateOperations")
 	db := appcontext.Database(ctx)
 
 	activeStatuses, err := database.FindActiveOperationStatus(db)
@@ -38,11 +38,23 @@ func UpdateOperations(ctx context.Context, epoch time.Time, chains []string) {
 			continue
 		}
 
-		addr, operation, err := getOperationInfos(db, status.OperationInfoID)
+		userID, addr, operation, err := getOperationInfos(db, status.OperationInfoID)
 		if err != nil {
 			log.WithError(err).
 				Error("Failed to getOperationInfos")
 			continue
+		}
+
+		accountID := uint64(addr.AccountID)
+		if operation.AssetID != 0 {
+			// create user asset account if needed
+			newAccountID, err := createUserAssetAccount(ctx, uint64(userID), uint64(addr.AccountID), operation.AssetID)
+			if err != nil {
+				log.WithError(err).
+					Error("Failed to createUserAssetAccount")
+				continue
+			}
+			accountID = newAccountID
 		}
 
 		// deposit amount to account
@@ -62,7 +74,7 @@ func UpdateOperations(ctx context.Context, epoch time.Time, chains []string) {
 				accountedStatus = "settled"
 			}
 		}
-		accountEntry, err := accountDeposit(ctx, uint64(addr.AccountID), uint64(operation.ID), float64(operation.Amount), "WalletDeposit")
+		accountEntry, err := accountDeposit(ctx, accountID, uint64(operation.ID), float64(operation.Amount), "WalletDeposit")
 		if err != nil {
 			log.WithError(err).
 				Error("Failed to AccountDeposit")
@@ -98,18 +110,96 @@ func UpdateOperations(ctx context.Context, epoch time.Time, chains []string) {
 	}).Info("Operations updated")
 }
 
-func getOperationInfos(db bank.Database, operationInfoID model.OperationInfoID) (model.CryptoAddress, model.OperationInfo, error) {
+func getOperationInfos(db bank.Database, operationInfoID model.OperationInfoID) (model.UserID, model.CryptoAddress, model.OperationInfo, error) {
 	// fetch OperationInfo from db
 	operation, err := database.GetOperationInfo(db, operationInfoID)
 	if err != nil {
-		return model.CryptoAddress{}, model.OperationInfo{}, err
+		return 0, model.CryptoAddress{}, model.OperationInfo{}, err
 	}
 
 	// fetch CryptoAddress from db
 	addr, err := database.GetCryptoAddress(db, operation.CryptoAddressID)
 	if err != nil {
-		return model.CryptoAddress{}, model.OperationInfo{}, err
+		return 0, model.CryptoAddress{}, model.OperationInfo{}, err
 	}
 
-	return addr, operation, nil
+	account, err := database.GetAccountByID(db, addr.AccountID)
+	if err != nil {
+		return 0, model.CryptoAddress{}, model.OperationInfo{}, err
+	}
+
+	return account.UserID, addr, operation, nil
+}
+
+func createUserAssetAccount(ctx context.Context, userID, accountID uint64, assetID model.AssetID) (uint64, error) {
+	log := logger.Logger(ctx).WithField("Method", "tasks.createUserAssetAccount")
+	db := appcontext.Database(ctx)
+
+	log = log.WithFields(logrus.Fields{
+		"UserID":    userID,
+		"AccountID": accountID,
+		"AssetID":   assetID,
+	})
+
+	// no asset, no error
+	if assetID == 0 {
+		return accountID, nil
+	}
+	if userID == 0 {
+		return 0, database.ErrInvalidUserID
+	}
+
+	// check if asset exists
+	asset, err := database.GetAsset(db, assetID)
+	if err != nil {
+		log.WithError(err).
+			Error("Asset NotFound")
+		return accountID, nil
+	}
+
+	// check if account exists
+	accounts, err := client.AccountList(ctx, userID)
+	if err != nil {
+		log.WithError(err).
+			Error("AccountList failed")
+		return accountID, nil
+	}
+
+	// find account with currency
+	for _, account := range accounts.Accounts {
+		if account.Currency.Name == string(asset.CurrencyName) {
+			log.
+				WithField("CurrencyName", asset.CurrencyName).
+				WithField("AccountID", account.AccountID).
+				Debug("Account Exists")
+			return account.AccountID, nil
+		}
+	}
+
+	// if currency does not exist try to create
+	creation, err := client.AccountCreate(ctx, userID, string(asset.CurrencyName))
+	if err != nil {
+		log.WithError(err).
+			Error("AccountCreate failed")
+		return 0, err
+	}
+	account := creation.Info
+	// curency is created and already available
+	if account.Status == "normal" {
+		return account.AccountID, nil
+	}
+
+	// activate currency
+	account, err = client.AccountSetStatus(ctx, account.AccountID, "normal")
+	if err != nil {
+		log.WithError(err).
+			Error("AccountSetStatus failed")
+		return 0, err
+	}
+
+	log.
+		WithField("NewAccountID", account.AccountID).
+		Debug("Account Created")
+
+	return account.AccountID, nil
 }
