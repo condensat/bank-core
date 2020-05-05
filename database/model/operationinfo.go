593777534b4bd70b81f2e183f7dfc2cf041a2ef5
Michel Foucault <michel@condensat.tech>
2020-05-05 16:00:00 +0000

    Add Asset to OperationInfo model
    - need by task UpdateOperations

--- database/model/operationinfo.go
+++ database/model/operationinfo.go
@@ -16,6 +16,7 @@ type OperationInfo struct {
 	CryptoAddressID CryptoAddressID `gorm:"index;not null"`                // [FK] Reference to CryptoAddress table
 	Timestamp       time.Time       `gorm:"index;not null;type:timestamp"` // Creation timestamp
 	TxID            TxID            `gorm:"unique_index;not null;size:64"` // Transaction ID
+	Asset           String          `gorm:"index;size:64;"`                // CryptoAddress optional asset, nullable, mutable`
 	Amount          Float           `gorm:"default:0.0;not null"`          // Operation amount (GTE 0.0)
 	Data            String          `gorm:"type:json;not null"`            // Specific operation json data
 }
