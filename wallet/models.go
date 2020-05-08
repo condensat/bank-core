614cd2eb258d75a318e8f2a9b988cf3980f6d99c
Michel Foucault <michel@condensat.tech>
2020-05-08 08:00:00 +0000

    Update wallet AssetModel

--- wallet/models.go
+++ wallet/models.go
@@ -13,6 +13,6 @@ func Models() []model.Model {
 	var result []model.Model
 	result = append(result, database.CryptoAddressModel()...)
 	result = append(result, database.OperationInfoModel()...)
-	result = append(result, new(model.Asset))
+	result = append(result, database.AssetModel()...)
 	return result
 }
