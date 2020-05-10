5cb45cce0ed3a091e9336a7a9974ddca885ae66d
Michel Foucault <michel@condensat.tech>
2020-05-10 13:00:00 +0000

    Return liquid deposit for all crypto
    - generate deferents deposit addresses for liquid assets

--- api/services/wallet.go
+++ api/services/wallet.go
@@ -84,7 +84,7 @@ func (p *WalletService) NextDeposit(r *http.Request, request *WalletNextDepositR
 			Error("Non Crypto Account")
 		return sessions.ErrInternalError
 	}
-	chain, err := getChainFromCurrencyName(account.Currency.Name)
+	chain, err := getChainFromCurrencyName(account.Currency.Crypto, account.Currency.Name)
 	if err != nil {
 		log.WithError(err).
 			WithField("AccountID", request.AccountID).
@@ -105,7 +105,7 @@ func (p *WalletService) NextDeposit(r *http.Request, request *WalletNextDepositR
 	}
 
 	// Reply
-	protocol, err := getProtocolFromCurrencyName(account.Currency.Name)
+	protocol, err := getProtocolFromCurrencyName(account.Currency.Crypto, account.Currency.Name)
 	*reply = WalletNextDepositResponse{
 		Currency:      account.Currency.Name,
 		PublicAddress: addr.PublicAddress,
@@ -128,7 +128,7 @@ func (p *WalletService) NextDeposit(r *http.Request, request *WalletNextDepositR
 	return nil
 }
 
-func getChainFromCurrencyName(currencyName string) (string, error) {
+func getChainFromCurrencyName(isCrypto bool, currencyName string) (string, error) {
 	switch currencyName {
 	case "BTC":
 		return "bitcoin-mainnet", nil
@@ -138,11 +138,14 @@ func getChainFromCurrencyName(currencyName string) (string, error) {
 		return "liquid-mainnet", nil
 
 	default:
+		if isCrypto {
+			return "liquid-mainnet", nil
+		}
 		return "", ErrWalletChainNotFoundError
 	}
 }
 
-func getProtocolFromCurrencyName(currencyName string) (string, error) {
+func getProtocolFromCurrencyName(isCrypto bool, currencyName string) (string, error) {
 	switch currencyName {
 	case "BTC":
 		return "bitcoin", nil
@@ -152,6 +155,9 @@ func getProtocolFromCurrencyName(currencyName string) (string, error) {
 		return "liquid", nil
 
 	default:
+		if isCrypto {
+			return "liquid", nil
+		}
 		return "", ErrWalletChainNotFoundError
 	}
 }
