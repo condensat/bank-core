e004ff7b0bd7b963d262ab02f51e7fcc346f7dbe
Michel Foucault <michel@condensat.tech>
2020-06-25 10:00:00 +0000

    Add bitcoin dumpprivkey command

--- wallet/bitcoin/commands/commands.go
+++ wallet/bitcoin/commands/commands.go
@@ -16,6 +16,7 @@ const (
 	CmdGetAddressInfo  = Command("getaddressinfo")
 	CmdSendMany        = Command("sendmany")
 
+	CmdDumpPrivkey          = Command("dumpprivkey")
 	CmdCreateRawTransaction = Command("createrawtransaction")
 	CmdDecodeRawTransaction = Command("decoderawtransaction")
 	CmdFundRawTransaction   = Command("fundrawtransaction")
