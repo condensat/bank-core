5d3c1997adb2b4dd8a65d533f219055c737191f6
Michel Foucault <michel@condensat.tech>
2020-07-13 08:00:00 +0000

    Move conversion helpers to common

--- accounting/handlers/accounttransfert.go
+++ accounting/handlers/accounttransfert.go
@@ -111,8 +111,8 @@ func AccountTransfer(ctx context.Context, transfer common.AccountTransfer) (comm
 
 	// Store operations
 	operations, err := database.AppendAccountOperationSlice(db,
-		convertEntryToOperation(transfer.Source),
-		convertEntryToOperation(transfer.Destination),
+		common.ConvertEntryToOperation(transfer.Source),
+		common.ConvertEntryToOperation(transfer.Destination),
 	)
 	if err != nil {
 		log.WithError(err).
@@ -132,8 +132,8 @@ func AccountTransfer(ctx context.Context, transfer common.AccountTransfer) (comm
 	log.Trace("Account transfer")
 
 	return common.AccountTransfer{
-		Source:      convertOperationToEntry(source, "N/A"),
-		Destination: convertOperationToEntry(destination, "N/A"),
+		Source:      common.ConvertOperationToEntry(source, "N/A"),
+		Destination: common.ConvertOperationToEntry(destination, "N/A"),
 	}, nil
 }
 
@@ -160,55 +160,4 @@ func OnAccountTransfer(ctx context.Context, subject string, message *bank.Messag
 			// return response
 			return &response, nil
 		})
-}
-
-// conversion helpers
-
-func convertOperationToEntry(op model.AccountOperation, label string) common.AccountEntry {
-	return common.AccountEntry{
-		OperationID: uint64(op.ID),
-		AccountID:   uint64(op.AccountID),
-		ReferenceID: uint64(op.ReferenceID),
-
-		OperationType:    string(op.OperationType),
-		SynchroneousType: string(op.SynchroneousType),
-
-		Timestamp: op.Timestamp,
-		Label:     label,
-		Amount:    float64(*op.Amount),
-		Balance:   float64(*op.Balance),
-
-		LockAmount:  float64(*op.LockAmount),
-		TotalLocked: float64(*op.TotalLocked),
-	}
-}
-
-func convertEntryToOperation(entry common.AccountEntry) model.AccountOperation {
-	amount := model.Float(entry.Amount)
-	lockAmount := model.Float(entry.LockAmount)
-
-	// Balance & totalLocked ar computed by database later, must be valid for pre-check
-	var balance model.Float
-	if balance < amount {
-		balance = amount
-	}
-	var totalLocked model.Float
-	if totalLocked < lockAmount {
-		totalLocked = lockAmount
-	}
-
-	return model.AccountOperation{
-		AccountID:        model.AccountID(entry.AccountID),
-		SynchroneousType: model.ParseSynchroneousType(entry.SynchroneousType),
-		OperationType:    model.ParseOperationType(entry.OperationType),
-		ReferenceID:      model.RefID(entry.ReferenceID),
-
-		Amount:  &amount,
-		Balance: &balance,
-
-		LockAmount:  &lockAmount,
-		TotalLocked: &totalLocked,
-
-		Timestamp: entry.Timestamp,
-	}
 }
