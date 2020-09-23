d36198dbba99bec470b700eb4c0494fa0211fb08
Michel Foucault <michel@condensat.tech>
2020-09-23 10:00:00 +0000

    Start json-rpc server with Dashboard service
    - Add ConnContext
    - Register services with cors handler
    - Dashboard is empty (panic)

--- backoffice/backoffice.go
+++ backoffice/backoffice.go
@@ -6,17 +6,49 @@ package backoffice
 
 import (
 	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/condensat/bank-core/backoffice/services"
 
 	"github.com/condensat/bank-core/logger"
 	"github.com/condensat/bank-core/utils"
 
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"github.com/urfave/negroni"
 )
 
 type BackOffice int
 
 func (p *BackOffice) Run(ctx context.Context, port int, corsAllowedOrigins []string) {
 	log := logger.Logger(ctx).WithField("Method", "backoffice.BackOffice.Run")
+
+	muxer := mux.NewRouter()
+
+	services.RegisterServices(ctx, muxer, corsAllowedOrigins)
+
+	handler := negroni.New(&negroni.Recovery{})
+	handler.UseHandler(muxer)
+
+	server := &http.Server{
+		Addr:           fmt.Sprintf(":%d", port),
+		Handler:        handler,
+		ReadTimeout:    3 * time.Second,
+		WriteTimeout:   15 * time.Second,
+		MaxHeaderBytes: 1 << 16, // 16 KiB
+		ConnContext:    func(conCtx context.Context, c net.Conn) context.Context { return ctx },
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.WithError(err).
+				Info("Http server exited")
+		}
+	}()
 
 	log.WithFields(logrus.Fields{
 		"Hostname": utils.Hostname(),
