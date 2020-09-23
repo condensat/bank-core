7e3d118b91c64fea98234123602e34cdde57b6ce
Michel Foucault <michel@condensat.tech>
2020-09-23 08:00:00 +0000

    Add BackOffice package & cmd

---
+++ backoffice/cmd/bank-backoffice/main.go
@@ -0,0 +1,77 @@
+// Copyright 2020 Condensat Tech. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"time"
+
+	"github.com/condensat/bank-core/appcontext"
+
+	"github.com/condensat/bank-core/backoffice"
+
+	"github.com/condensat/bank-core/cache"
+	"github.com/condensat/bank-core/database"
+	"github.com/condensat/bank-core/logger"
+	"github.com/condensat/bank-core/messaging"
+	"github.com/condensat/bank-core/monitor/processus"
+)
+
+type BackOffice struct {
+}
+
+type Args struct {
+	App appcontext.Options
+
+	Redis    cache.RedisOptions
+	Nats     messaging.NatsOptions
+	Database database.Options
+
+	BackOffice BackOffice
+}
+
+func parseArgs() Args {
+	var args Args
+
+	appcontext.OptionArgs(&args.App, "BackOffice")
+
+	cache.OptionArgs(&args.Redis)
+	messaging.OptionArgs(&args.Nats)
+	database.OptionArgs(&args.Database)
+
+	flag.Parse()
+
+	return args
+}
+
+func main() {
+	args := parseArgs()
+
+	ctx := context.Background()
+	ctx = appcontext.WithOptions(ctx, args.App)
+	ctx = appcontext.WithHasherWorker(ctx, args.App.Hasher)
+	ctx = appcontext.WithCache(ctx, cache.NewRedis(ctx, args.Redis))
+	ctx = appcontext.WithWriter(ctx, logger.NewRedisLogger(ctx))
+	ctx = appcontext.WithMessaging(ctx, messaging.NewNats(ctx, args.Nats))
+	ctx = appcontext.WithDatabase(ctx, database.NewDatabase(args.Database))
+	ctx = appcontext.WithProcessusGrabber(ctx, processus.NewGrabber(ctx, 15*time.Second))
+
+	migrateDatabase(ctx)
+
+	var backOffice backoffice.BackOffice
+	backOffice.Run(ctx)
+}
+
+func migrateDatabase(ctx context.Context) {
+	db := appcontext.Database(ctx)
+
+	err := db.Migrate(backoffice.Models())
+	if err != nil {
+		logger.Logger(ctx).WithError(err).
+			WithField("Method", "main.migrateDatabase").
+			Panic("Failed to migrate backoffice models")
+	}
+}
