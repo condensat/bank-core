38dc67ae7956385e28ead187eff1dc0bef65f485
Michel Foucault <michel@condensat.tech>
2020-09-29 12:00:00 +0000

    Update BackOffice Models

--- backoffice/models.go
+++ backoffice/models.go
@@ -12,5 +12,18 @@ func Models() []model.Model {
 	return []model.Model{
 		new(model.User),
 		new(model.UserRole),
+
+		new(model.Account),
+		new(model.AccountState),
+		new(model.AccountOperation),
+
+		new(model.OperationStatus),
+		new(model.Batch),
+		new(model.BatchInfo),
+		new(model.Withdraw),
+		new(model.WithdrawInfo),
+
+		new(model.SwapInfo),
+		new(model.SwapInfo),
 	}
 }
