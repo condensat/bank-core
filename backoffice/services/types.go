015ae593795e2cb8f94652d2252cfe818a58898d
Michel Foucault <michel@condensat.tech>
2020-10-06 15:00:00 +0000

    Add Page & CountPerPage RequestPaging
    - Used by client to compute id

--- backoffice/services/types.go
+++ backoffice/services/types.go
@@ -5,6 +5,9 @@
 package services
 
 type RequestPaging struct {
-	Page      int `json:"page"`
-	PageCount int `json:"pageCount"`
+	Page         int    `json:"page"`
+	PageCount    int    `json:"pageCount"`
+	CountPerPage int    `json:"countPerPage"`
+	Start        string `json:"start"`
+	Next         string `json:"next"`
 }
